5546359878e5125b61044701033313fbc9a6f542
Luis Anchante <92268396+LuisAnchante@users.noreply.github.com>
2023-11-17 13:16:59 -0500

    Update ApplicationSentimientos.py

--- ApplicationSentimientos.py
+++ ApplicationSentimientos.py
@@ -121,5 +121,5 @@ def analyze_sentiments_nb(nb_clicks, svm_clicks, rf_clicks, nb_user_text, svm_us
 
 # Iniciar la aplicación
 if __name__ == "__main__":
-    #app.run_server(debug=True)
-    app.run_server(debug=False)
+    app.run_server(debug=True)
+    #app.run_server(debug=False)
